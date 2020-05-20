9d7d184996fd3b4d65c7f253fc1c15a73ff3a950
Tyler Ferguson <tyler.ferguson@ndm.ox.ac.uk>
2020-05-21 00:00:18 +0100

    Added data-raw/player_totals.R. Generate data/player_totals

---
+++ data-raw/player_totals.R
@@ -0,0 +1,14 @@
+raw_player_totals <- read_csv("inst/extdata/bref_player_totals.csv")
+
+group_positions <- raw_player_totals %>%
+  count(slugPlayerBREF, groupPosition) %>%
+  arrange(desc(n)) %>%
+  group_by(slugPlayerBREF) %>%
+  summarise(
+    MostPlayedGroupPosition = first(groupPosition)
+  )
+
+player_totals <- raw_player_totals %>%
+  left_join(group_positions, by = "slugPlayerBREF")
+
+usethis::use_data(player_totals, overwrite = TRUE)
